5f89a0898339755e8671a2ca1ca4d06b83911685
lavvy <lavashonline@gmail.com>
2016-03-21 08:14:54 -0700

    edited to suit gogs format

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [ ! -e ${HTTP_DOCUMENTROOT}/index.php ]; then
+   echo "=> Installing sqlbuddy in ${HTTP_DOCUMENTROOT}/ - this may take a while ..."
+   touch ${HTTP_DOCUMENTROOT}/index.php
+   wget -O /tmp/sqlbuddy.tar.gz ${SQLBUDDY_URL}
+   tar -zxf /tmp/sqlbuddy.tar.gz -C /tmp/
+   cp -a /tmp/src/. ${HTTP_DOCUMENTROOT}/
+   #rm -rf /tmp/sqlbuddy-*
+   chown -R www-data:www-data ${HTTP_DOCUMENTROOT}
+fi
+
 VOLUME_HOME="/var/lib/mysql"
 
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
