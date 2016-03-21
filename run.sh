452ef182da911daafdfbb066a5ad3e8d4284267c
lavvy <lavashonline@gmail.com>
2016-03-21 10:24:01 -0700

    add initializer

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ ! -e ${HTTP_DOCUMENTROOT}/index.php ]; then
+if [ ! -e ${HTTP_DOCUMENTROOT}/index1.php ]; then
    echo "=> Installing sqlbuddy in ${HTTP_DOCUMENTROOT}/ - this may take a while ..."
-   touch ${HTTP_DOCUMENTROOT}/index.php
+   touch ${HTTP_DOCUMENTROOT}/index1.php
    wget -O /tmp/sqlbuddy.tar.gz ${SQLBUDDY_URL}
    tar -zxf /tmp/sqlbuddy.tar.gz -C /tmp/
    cp -a /tmp/src/. ${HTTP_DOCUMENTROOT}/
@@ -14,6 +14,14 @@ VOLUME_HOME="/var/lib/mysql"
 
 sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
     -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
-
+if [[ ! -d $VOLUME_HOME/mysql ]]; then
+    echo "=> An empty or uninitialized MySQL volume is detected in $VOLUME_HOME"
+    echo "=> Installing MySQL ..."
+    mysql_install_db > /dev/null 2>&1
+    echo "=> Done!"  
+    /create_mysql_admin_user.sh
+else
+    echo "=> Using an existing volume of MySQL"
+fi
 
 exec supervisord -n
