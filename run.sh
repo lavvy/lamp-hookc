84d550a61420667a5f043216791017ee20801ae9
lavvy <lavashonline@gmail.com>
2016-03-21 09:10:35 -0700

    edited to suit gogs format

--- run.sh
+++ run.sh
@@ -10,3 +10,10 @@ if [ ! -e ${HTTP_DOCUMENTROOT}/index.php ]; then
    chown -R www-data:www-data ${HTTP_DOCUMENTROOT}
 fi
 
+VOLUME_HOME="/var/lib/mysql"
+
+sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
+    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/apache2/php.ini
+
+
+exec supervisord -n
